c6a58d831c0c43b76a485ba0c8b5841b8fcebcff
Alexander Madsen <akmadsen522@gmail.com>
2018-09-27 12:39:55 -0600

    Now have the file locations compliant with the agreed on architecture

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@ var sass = require('gulp-sass');
 gulp.task('default', ['watch']); 
 
 gulp.task('style', function() {
-    gulp.src('./sass/app.scss')
+    gulp.src('./src/sass/app.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./public/css'));
 }); 
 
 gulp.task('watch', function() {
     // Any folder or file with .scss, run the 'style' task
-    gulp.watch('./sass/**/*.scss', ['style']); 
+    gulp.watch('./src/sass/**/*.scss', ['style']); 
 }); 
