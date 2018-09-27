3c45f0d041b619f3895a0dcababa2657644857b1
Alexander Madsen <akmadsen522@gmail.com>
2018-09-27 12:18:33 -0600

    Added in gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'); // Require is Node specific, assumes Node Modules
+var sass = require('gulp-sass'); 
+
+gulp.task('test', function() {
+    console.log('testing...'); 
+}); 
+
+gulp.task('style', function() {
+    gulp.src('./sass/app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'));
+}); 
+
+gulp.task('watch', function() {
+    // Any folder or file with .scss, run the 'style' task
+    gulp.watch('./sass/**/*.scss', ['style']); 
+}); 
